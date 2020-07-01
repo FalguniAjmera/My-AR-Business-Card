cf20157647759b920d2f43b601d5c84c9ac56de3
FalguniAjmera <43901389+FalguniAjmera@users.noreply.github.com>
2020-07-01 15:41:11 +0530

    First commit
    unity project added to github

---
+++ AR Business Card/Assets/vb_anim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler
+{
+	public GameObject vbBtnObj;
+	public Animator player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("button 1");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        player.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+    	player.Play("WalkRight");
+    	Debug.Log("BTN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+    	player.Play("none");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
